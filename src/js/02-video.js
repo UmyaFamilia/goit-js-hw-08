52513702112ba2b352acc24f6684ec0c89488843
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-19 16:18:31 +0300

    4commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
 
+var _ = require('lodash');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+let startPoint = Number(localStorage.getItem('videoplayer-current-time')) || 0;
+
+player.setCurrentTime(startPoint);
+
+player.on('timeupdate', _.throttle(DontKnowHowToCallIt, 1000));
+
+function DontKnowHowToCallIt(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
