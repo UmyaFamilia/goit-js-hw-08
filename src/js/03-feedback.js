6e1e6b644945a8d141795cf0ceb61aea5ad70e62
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-19 16:34:22 +0300

    5commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const inpute = document.querySelector('[name="email"]');
+
+const textarea = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+let info = {
+  name: '',
+  message: '',
+};
+if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+  info.name = JSON.parse(localStorage.getItem('feedback-form-state')).name;
+  info.message = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
+  inpute.value = info.name;
+  textarea.value = info.message;
+}
+
+form.addEventListener('input', throttle(IDontKnow, 500));
+function IDontKnow(evnt) {
+  if (evnt.target.name == 'email') {
+    info.name = evnt.target.value;
+  }
+  if (evnt.target.name == 'message') {
+    info.message = evnt.target.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(info));
+}
+form.addEventListener('submit', end);
+function end(evnt) {
+  evnt.preventDefault();
+  console.log('email= ' + inpute.value + '    maessage= ' + textarea.value);
+  localStorage.clear();
+  inpute.value = '';
+  textarea.value = '';
+}
